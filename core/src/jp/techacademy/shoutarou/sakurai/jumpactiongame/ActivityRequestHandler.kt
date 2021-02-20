dd0c0695ac64c119fcf5209b39c33117948220ea
Shoutarou.Sakurai <naritake109@yahoo.ne.jp>
2021-02-20 20:27:13 +0900

    Initial commit

---
+++ core/src/jp/techacademy/shoutarou/sakurai/jumpactiongame/ActivityRequestHandler.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.shoutarou.sakurai.jumpactiongame
+
+interface ActivityRequestHandler {
+    fun showAds(show: Boolean)
+}
---
+++ core/src/jp/techacademy/shoutarou/sakurai/jumpactiongame/JumpActionGame.kt
@@ -0,0 +1,20 @@
+package jp.techacademy.shoutarou.sakurai.jumpactiongame
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.utils.ScreenUtils
+
+class JumpActionGame(val mRequestHandler: ActivityRequestHandler) : Game() {
+    lateinit var batch: SpriteBatch
+
+    override fun create() {
+        batch = SpriteBatch()
+
+        // GameScreenを表示する
+        setScreen(GameScreen(this))
+    }
+
+
+}//class
